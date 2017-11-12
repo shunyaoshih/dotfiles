b650e526a8b8a2240ad979c5b6615c2a54db2562
gantheory <b03901133@ntu.edu.tw>
2017-11-12 22:00:06 +0800

    default app for opening pdf: skim

--- scripts/change_default_apps.sh
+++ scripts/change_default_apps.sh
@@ -8,5 +8,6 @@ duti -s org.vim.MacVim .h all
 duti -s org.vim.MacVim .java all
 duti -s org.vim.MacVim .md all
 duti -s org.vim.MacVim .py all
+duti -s net.sourceforge.skim-app.skim .pdf all
 duti -s org.vim.MacVim .txt all
 duti -s cx.c3.theunarchiver .zip all
