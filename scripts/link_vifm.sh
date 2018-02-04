fa492a9c615e579b29ec27b0eb7889f123c0cd44
gantheory <gantheory@gantheorys-MacBook-Pro.local>
2018-02-05 03:28:48 +0800

    fix bugs of zshrc and link_vifm.sh

--- scripts/link_vifm.sh
+++ scripts/link_vifm.sh
@@ -4,5 +4,7 @@ source ./utils.sh
 SOURCE=$HOME'/Github/dotfiles'
 
 print 'Link Vifm'
+mkdir $HOME'/.vifm'
+mkdir $HOME'/.vifm/colors'
 link_file $SOURCE'/vifm/vifmrc' $HOME'/.vifm/vifmrc'
 link_file $SOURCE'/vifm/Gantheory.vifm' $HOME'/.vifm/colors/Gantheory.vifm'
