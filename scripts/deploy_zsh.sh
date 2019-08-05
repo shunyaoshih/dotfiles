3cb26d16bd3e3cc8be8ff7717852281b24174c8d
gantheory <shunyaoshih@gmail.com>
2019-08-05 23:51:15 +0800

    deploying zsh with ctags installation

--- scripts/deploy_zsh.sh
+++ scripts/deploy_zsh.sh
@@ -2,6 +2,7 @@
 source ./utils.sh
 
 print 'Deploy Zsh'
+brew_install '--HEAD universal-ctags'
 brew_install 'zsh'
 brew_install 'diff-so-fancy'
 run_cmd 'git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"'
