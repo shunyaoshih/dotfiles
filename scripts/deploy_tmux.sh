fb36e67f092ae4b4d33916702e6edd2f4172d480
gantheory <shunyaoshih@gmail.com>
2019-08-06 00:07:09 +0800

    fix dependencies for zsh and tmux

--- scripts/deploy_tmux.sh
+++ scripts/deploy_tmux.sh
@@ -4,3 +4,10 @@ source ./utils.sh
 print 'Deploy tmux'
 brew_install 'reattach-to-user-namespace'
 brew_install 'tmux'
+run_cmd 'git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm'
+
+SOURCE=$HOME'/Github/dotfiles'
+
+print 'Link Tmux'
+link_file $SOURCE'/tmux/tmux.conf' $HOME'/.tmux.conf'
+link_file $SOURCE'/tmux/tmux_osx.conf' $HOME'/.tmux_osx.conf'
--- scripts/deploy_zsh.sh
+++ scripts/deploy_zsh.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 source ./utils.sh
 
+SOURCE=$HOME'/Github/dotfiles'
+
 print 'Deploy Zsh'
 
 print 'Homebrew Tap universal-ctags/universal-ctags'
@@ -11,3 +13,6 @@ brew_install 'zsh'
 brew_install 'diff-so-fancy'
 run_cmd 'git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"'
 print 'Remember to source ~/.zshrc'
+
+print 'Link Zsh'
+link_file $SOURCE'/zsh/zshrc' $HOME'/.zshrc'
