f0a24568a736caf1da4bd92b44213288837209f4
gantheory <shunyaoshih@gmail.com>
2019-08-06 00:00:46 +0800

    fix dependencies for zsh

--- scripts/deploy_zsh.sh
+++ scripts/deploy_zsh.sh
@@ -2,7 +2,11 @@
 source ./utils.sh
 
 print 'Deploy Zsh'
+
+print 'Homebrew Tap universal-ctags/universal-ctags'
+run_cmd 'brew tap universal-ctags/universal-ctags'
 brew_install '--HEAD universal-ctags'
+
 brew_install 'zsh'
 brew_install 'diff-so-fancy'
 run_cmd 'git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"'
