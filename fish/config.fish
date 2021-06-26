ceea791fc4109ff34da05a62fc0f9e40a2c289db
gantheory <shunyaoshih@gmail.com>
2021-06-25 20:48:00 -0700

    Add Fish configs.

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+# Vim binding in Fish.
+fish_vi_key_bindings
+
+# Alias.
+alias md=mkdir
+alias v=nvim
+alias vinit='v ~/.config/nvim/init.vim'
+alias tinit='v ~/.tmux.conf'
+alias st='tmux source ~/.tmux.conf'
+alias finit='v ~/.config/fish/config.fish'
+
+# Remove default Fish greeting.
+set fish_greeting
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,25 @@
+# Defined interactively
+function fish_prompt --description 'Informative prompt'
+    # Save the return status of the previous command
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+    if functions -q fish_is_root_user; and fish_is_root_user
+        printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
+                                                         and set_color $fish_color_cwd_root
+                                                         or set_color $fish_color_cwd) \
+            (prompt_pwd) (set_color normal)
+    else
+        set -l pipestatus_string (__fish_print_pipestatus "[" "] " "|" (set_color $fish_color_status) \
+                                  (set_color --bold $fish_color_status) $last_pipestatus)
+
+        printf '%s[%s] %s%s%s %s@%s %s\n> ' \
+          (set_color 808080) (date "+%H:%M:%S") \
+          (set_color fffc87) (prompt_pwd) $pipestatus_string \
+          (set_color 6c6c6c) $USER (prompt_hostname) \
+          (set_color normal)
+    end
+end
+
+function fish_mode_prompt; end
+funcsave fish_mode_prompt
