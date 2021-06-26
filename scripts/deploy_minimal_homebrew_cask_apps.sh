3be649d1a67ada88acc7e996dcb702db45113f8b
gantheory <shunyaoshih@gmail.com>
2021-06-25 21:11:21 -0700

    Update scripts.

--- scripts/deploy_minimal_homebrew_cask_apps.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-source ./utils.sh
-
-print 'Homebrew Tap Cask'
-run_cmd 'brew tap caskroom/cask'
-
-print 'Install Minimal Homebrew-Cask Apps'
-cask_install 'iterm2'
--- scripts/install_homebrew_cask_apps.sh
+++ scripts/install_homebrew_cask_apps.sh
@@ -1,20 +1,12 @@
 #! /bin/bash
 source ./utils.sh
 
-print 'Homebrew Tap Cask'
-run_cmd 'brew tap caskroom/cask'
-
-print 'Install Homebrew-Cask Apps'
+print 'Install Homebrew Cask Apps'
 cask_install 'alfred'
-cask_install 'bettertouchtool'
 cask_install 'google-chrome'
 cask_install 'google-backup-and-sync'
 cask_install 'istat-menus'
 cask_install 'iterm2'
-cask_install 'java'
-# cask_install 'mactex'
-cask_install 'skype'
-cask_install 'slack'
 cask_install 'spectacle'
 cask_install 'spotify'
 cask_install 'steam'
