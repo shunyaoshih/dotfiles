d36118d0da92221df2587cff34174fdad431b32b
gantheory <shunyaoshih@gmail.com>
2022-04-07 21:52:11 -0700

    Add git alias.

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,10 @@ alias tinit='v ~/.tmux.conf'
 alias st='tmux source ~/.tmux.conf'
 alias finit='v ~/.config/fish/config.fish'
 alias lla='ls -a -l'
+alias gs='git status'
+alias gd='git diff'
+alias ga='git add'
+alias gcm='git commit -m'
 
 # Remove default Fish greeting.
 set fish_greeting
