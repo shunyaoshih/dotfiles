de3f293e545605415e9ffe1610c2aac92d1e7568
gantheory <shunyaoshih@gmail.com>
2022-04-11 16:40:40 -0700

    Update fish color to be close to pikacode.

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -2,19 +2,19 @@ function fish_mode_prompt
   set_color --bold 000000
   switch $fish_bind_mode
     case default
-      set_color -b 3688f7
+      set_color -b 0080CB
       echo '[N]'
     case insert
-      set_color -b a7e069
+      set_color -b 87D700
       echo '[I]'
     case replace_one
-      set_color -b d77d71
+      set_color -b FF548C
       echo '[R]'
     case visual
-      set_color -b f4b13e
+      set_color -b FFAF5F
       echo '[V]'
     case '*'
-      set_color -b red
+      set_color -b 9F0036
       echo '?'
   end
   set_color normal
