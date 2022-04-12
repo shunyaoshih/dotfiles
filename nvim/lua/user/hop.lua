fd8ca7616378e7b34fdcda970b489f1e483bb4c3
gantheory <shunyaoshih@gmail.com>
2022-04-11 20:45:11 -0700

    Add keymaps to make hop.nvim work in visual modes.

--- nvim/lua/user/hop.lua
+++ nvim/lua/user/hop.lua
@@ -9,4 +9,10 @@ local keymap = vim.api.nvim_set_keymap
 keymap("n", "<leader>hw", ":HopChar2<CR>", opts)
 keymap("n", "<leader>hl", ":HopLine<CR>", opts)
 
+keymap("v", "<leader>hw", "<CMD>HopChar2<CR>", opts)
+keymap("v", "<leader>hl", "<CMD>HopLine<CR>", opts)
+
+keymap("x", "<leader>hw", "<CMD>HopChar2<CR>", opts)
+keymap("x", "<leader>hl", "<CMD>HopLine<CR>", opts)
+
 hop.setup {}
