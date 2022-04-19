b74b92c1b1364613a938e68a9406c5b6d9b42271
gantheory <shunyaoshih@gmail.com>
2022-04-19 16:50:11 -0700

    Remove Golang plugin settings.

--- nvim/lua/user/go.lua
+++
@@ -1,8 +0,0 @@
-vim.g.go_fmt_autosave = 0
-vim.g.go_gopls_enabled = 0
-vim.g.go_imports_autosave = 0
-vim.g.go_mod_fmt_autosave = 0
-vim.g.go_metalinter_autosave_enabled = {}
-vim.g.go_highlight_extra_types = 1
-vim.g.go_highlight_operators = 1
-vim.g.go_highlight_functions = 1
