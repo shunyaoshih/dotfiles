2016229fe12eb45eb03dd572d38e0ddc5e6e8767
gantheory <shunyaoshih@gmail.com>
2022-04-21 19:48:27 -0700

    Update alpha.

--- nvim/lua/user/alpha.lua
+++ nvim/lua/user/alpha.lua
@@ -4,4 +4,12 @@ if not ok then return end
 local ok, dashboard = pcall(require, "alpha.themes.dashboard")
 if not ok then return end
 
+dashboard.section.buttons.val = {
+    dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("c", "  Configuration",
+                     ":e ~/.config/nvim/init.vim <CR>"),
+    dashboard.button("q", "  Quit Neovim", ":qa<CR>")
+}
+
 alpha.setup(dashboard.config)
