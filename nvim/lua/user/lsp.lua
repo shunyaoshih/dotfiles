14c9a5ad24ae986462ab6291cece3f99a8de763d
gantheory <shunyaoshih@gmail.com>
2022-10-11 18:41:24 -0700

    Add diagnostic key mapping.

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -36,18 +36,15 @@ local on_attach = function(_, bufnr)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
     vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder,
-                   bufopts)
-    vim.keymap.set("n", "<leader>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
     vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
     vim.keymap.set("n", "<leader>f",
                    function() vim.lsp.buf.format {async = true} end, bufopts)
+
+    vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, bufopts)
+    vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, bufopts)
 end
 
 local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
