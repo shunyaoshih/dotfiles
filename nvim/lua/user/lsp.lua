385159fd1c90f5a2fe280c5c6ecaea54c99e69c9
gantheory <shunyaoshih@gmail.com>
2022-10-13 22:05:30 -0700

    Remove useless null-ls rustfmt setup.

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -1,6 +1,6 @@
 local border_ok, border = pcall(require, "user.border")
 if not border_ok then
-  return
+	return
 end
 
 local mason_ok, mason = pcall(require, "mason")
@@ -131,6 +131,5 @@ null_ls.setup({
 
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.rustfmt,
 	},
 })
