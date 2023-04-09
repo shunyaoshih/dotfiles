0905af2332209501cc69c6772730828cefd1e920
shunyaoshih <shunyaoshih@gmail.com>
2023-04-08 22:29:22 -0700

    Add VeryLazy event.

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -1,11 +1,21 @@
 return {
-  { "djoshea/vim-autoread" },
-  { "tpope/vim-repeat" },
+  {
+    "djoshea/vim-autoread",
+    event = "VeryLazy",
+  },
+  {
+    "tpope/vim-repeat",
+    event = "VeryLazy",
+  },
   { "tpope/vim-surround" },
-  { "wakatime/vim-wakatime" },
+  {
+    "wakatime/vim-wakatime",
+    event = "VeryLazy",
+  },
   {
     'Wansmer/treesj',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    event = "VeryLazy",
     config = function()
       require('treesj').setup({})
     end,
