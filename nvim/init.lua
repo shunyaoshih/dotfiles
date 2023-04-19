af89c2dd09cf3a9917e4d572601b2840e0057481
shunyaoshih <shunyaoshih@gmail.com>
2023-04-19 12:18:04 -0700

    Use local.lua instead of local.vim.

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ require("user.options")
 require("user.autocmds")
 require("user.keymaps")
 require("user.custom_functions")
+require("user.local")
 require("user.lazy")
 
 vim.api.nvim_create_autocmd("VimEnter", { command = "runtime custom/local.vim" })
